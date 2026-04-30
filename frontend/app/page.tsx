93271040c5c7e385e3e27baf1d1df4c7e0cc1288
Bennett <chamberlain.bennett@gmail.com>
2026-04-30 02:14:50 -0700

    style: desktop logo marginTop 23px

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -103,7 +103,7 @@ export default function IndexPage() {
             priority
           />
         </div>
-        <div className="hidden md:block" style={{ marginTop: '21px' }}>
+        <div className="hidden md:block" style={{ marginTop: '23px' }}>
           <Image
             src="/assets/images/superhotfin.png"
             alt="Superhot Fabrication Logo"
